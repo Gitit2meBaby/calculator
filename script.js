2a6f3e2a449c5b5c0039bb2614597cd15af6182d
Gitit2meBaby <thisworldofdans@gmail.com>
2023-09-22 01:14:36 +0700

    finished the styling and theme switch, then killed the calculator function somehow whilst trying to get the decimal point to work. dont for the night..

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-const thumb1 = document.querySelector("#thumb1");
-const thumb2 = document.querySelector("#thumb2");
-const thumb3 = document.querySelector("#thumb3");
-
-const btnReset = document.querySelector("#btnReset");
-
-
-
-
-
+// NUMBER BUTTON INPUTS
 const numberBtns = document.querySelectorAll('.number-btn');
 const display = document.querySelector("#display");
 let initialZeroReplaced = false;
 
-
-// NUMBER BUTTON INPUTS
 numberBtns.forEach(function (button) {
     button.addEventListener('click', () => {
         const buttonText = button.textContent;
 
-        if (!initialZeroReplaced) {
+        if (buttonText === '.') {
+            // Check if a decimal point already exists in the display content
+            if (!display.textContent.includes('.')) {
+                display.textContent += buttonText;
+
+            }
+        } else if (!initialZeroReplaced || isLastCalculation) {
             display.textContent = buttonText;
             initialZeroReplaced = true;
         } else {
@@ -26,6 +21,7 @@ numberBtns.forEach(function (button) {
         }
     });
 });
+
 
 // DELETE BUTTON
 const deleteButton = document.querySelector("#delete");
@@ -39,33 +35,85 @@ deleteButton.addEventListener('click', () => {
     }
 });
 
-
 // MATH BUTTONS
-const mathBtns = document.querySelectorAll('.math-btn')
-let isStoredNumber = false
-let storedNumber
+const mathBtns = document.querySelectorAll('.math-btn');
+let isStoredNumber = false;
+let storedNumber;
 
 mathBtns.forEach(function (button) {
     button.addEventListener('click', () => {
         const mathInput = button.textContent;
+
+        // Replace "x" with "*" in the storedNumber string
         if (!isStoredNumber) {
-            storedNumber = display.textContent + ' ' + mathInput
-            isStoredNumber = true
+            storedNumber = display.textContent + ' ' + mathInput.replace(/x/g, '*');
+            isStoredNumber = true;
         } else {
-            storedNumber = storedNumber + ' ' + display.textContent + ' ' + mathInput
+            storedNumber = storedNumber + ' ' + display.textContent + ' ' + mathInput.replace(/x/g, '*');
         }
-        display.textContent = '0'
-        initialZeroReplaced = false
-        console.log(storedNumber)
+
+        display.textContent = '0';
+        initialZeroReplaced = false;
+        console.log(storedNumber);
     });
 });
 
 // EQUALS BUTTON 
 const btnEquals = document.querySelector("#btnEquals");
 let calculatedNumber
+let isLastCalculation = false
 
 btnEquals.addEventListener('click', () => {
     let calculation = storedNumber + ' ' + display.textContent
     calculatedNumber = eval(calculation);
     display.textContent = calculatedNumber
+    console.log(storedNumber)
+    storedNumber = ''
+    isLastCalculation = true
 })
+
+// RESET BUTTON 
+const btnReset = document.querySelector("#btnReset");
+
+btnReset.addEventListener('click', () => {
+    display.textContent = '0'
+    storedNumber = ''
+    initialZeroReplaced = false
+    console.log(storedNumber)
+})
+
+// THEME SWITCHER
+const body = document.querySelector('.body')
+const thumb1 = document.querySelector("#thumb1");
+const thumb2 = document.querySelector("#thumb2");
+const thumb3 = document.querySelector("#thumb3");
+
+thumb1.addEventListener('click', () => {
+    thumb1.style.opacity = '1'
+    thumb2.style.opacity = '0'
+    thumb3.style.opacity = '0'
+    body.classList.remove('theme2')
+    body.classList.remove('theme3')
+    body.classList.add('theme1')
+})
+thumb2.addEventListener('click', () => {
+    thumb1.style.opacity = '0'
+    thumb2.style.opacity = '1'
+    thumb3.style.opacity = '0'
+    body.classList.remove('theme1')
+    body.classList.remove('theme3')
+    body.classList.add('theme2')
+})
+thumb3.addEventListener('click', () => {
+    thumb1.style.opacity = '0'
+    thumb2.style.opacity = '0'
+    thumb3.style.opacity = '1'
+    body.classList.remove('theme1')
+    body.classList.remove('theme2')
+    body.classList.add('theme3')
+})
+
+
+
+
+
