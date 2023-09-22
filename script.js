b7246dead6b18d94dd54548c95a043f3393aabdf
Gitit2meBaby <thisworldofdans@gmail.com>
2023-09-22 13:14:26 +0700

    Seemingly bug free calculator now

--- script.js
+++ script.js
@@ -1,119 +1,95 @@
 // NUMBER BUTTON INPUTS
 const numberBtns = document.querySelectorAll('.number-btn');
 const display = document.querySelector("#display");
-let initialZeroReplaced = false;
+let isStoredNumber = false;
+let storedNumber = '';
 
 numberBtns.forEach(function (button) {
     button.addEventListener('click', () => {
-        const buttonText = button.textContent;
-
-        if (buttonText === '.') {
-            // Check if a decimal point already exists in the display content
-            if (!display.textContent.includes('.')) {
-                display.textContent += buttonText;
-
-            }
-        } else if (!initialZeroReplaced || isLastCalculation) {
-            display.textContent = buttonText;
-            initialZeroReplaced = true;
-        } else {
-            display.textContent += buttonText;
+        lastCalculation = ''
+        if (isLastCalculation) {
+            display.value = '';
+            storedNumber = '';
+            isLastCalculation = false;
         }
+        const buttonText = button.textContent;
+        display.value += buttonText.trim();
     });
 });
-
 
 // DELETE BUTTON
 const deleteButton = document.querySelector("#delete");
 
 deleteButton.addEventListener('click', () => {
-    let currentText = display.textContent;
+    let currentText = display.value;
 
     if (currentText.length > 0) {
         currentText = currentText.slice(0, -1);
-        display.textContent = currentText;
+        display.value = currentText;
     }
 });
 
 // MATH BUTTONS
 const mathBtns = document.querySelectorAll('.math-btn');
-let isStoredNumber = false;
-let storedNumber;
 
 mathBtns.forEach(function (button) {
     button.addEventListener('click', () => {
         const mathInput = button.textContent;
 
-        // Replace "x" with "*" in the storedNumber string
-        if (!isStoredNumber) {
-            storedNumber = display.textContent + ' ' + mathInput.replace(/x/g, '*');
-            isStoredNumber = true;
-        } else {
-            storedNumber = storedNumber + ' ' + display.textContent + ' ' + mathInput.replace(/x/g, '*');
+        if (isLastCalculation) {
+            isLastCalculation = false;
         }
 
-        display.textContent = '0';
-        initialZeroReplaced = false;
+        if (lastCalculation) {
+            storedNumber = lastCalculation + mathInput.replace(/x/g, '*');
+        } else {
+            if (!isStoredNumber) {
+                storedNumber = display.value + mathInput.replace(/x/g, '*');
+                isStoredNumber = true;
+            } else {
+                storedNumber = storedNumber + display.value + mathInput.replace(/x/g, '*');
+            }
+        }
+
+        display.value = '';
         console.log(storedNumber);
     });
 });
 
-// EQUALS BUTTON 
+// EQUALS BUTTON
 const btnEquals = document.querySelector("#btnEquals");
-let calculatedNumber
-let isLastCalculation = false
+let calculatedNumber;
+let lastCalculation = '';
+let isLastCalculation = false;
 
 btnEquals.addEventListener('click', () => {
-    let calculation = storedNumber + ' ' + display.textContent
+    let calculation = storedNumber + display.value;
     calculatedNumber = eval(calculation);
-    display.textContent = calculatedNumber
-    console.log(storedNumber)
-    storedNumber = ''
-    isLastCalculation = true
-})
+    display.value = calculatedNumber.toLocaleString();
+    storedNumber = calculation;
+    isLastCalculation = true;
+    lastCalculation = calculation
+});
 
-// RESET BUTTON 
+// RESET BUTTON
 const btnReset = document.querySelector("#btnReset");
 
 btnReset.addEventListener('click', () => {
-    display.textContent = '0'
-    storedNumber = ''
-    initialZeroReplaced = false
-    console.log(storedNumber)
-})
+    display.value = '';
+    storedNumber = '';
+    isLastCalculation = false;
+    lastCalculation = ''
+});
 
 // THEME SWITCHER
-const body = document.querySelector('.body')
-const thumb1 = document.querySelector("#thumb1");
-const thumb2 = document.querySelector("#thumb2");
-const thumb3 = document.querySelector("#thumb3");
+const body = document.querySelector('.body');
+const thumbs = document.querySelectorAll(".thumb");
 
-thumb1.addEventListener('click', () => {
-    thumb1.style.opacity = '1'
-    thumb2.style.opacity = '0'
-    thumb3.style.opacity = '0'
-    body.classList.remove('theme2')
-    body.classList.remove('theme3')
-    body.classList.add('theme1')
-})
-thumb2.addEventListener('click', () => {
-    thumb1.style.opacity = '0'
-    thumb2.style.opacity = '1'
-    thumb3.style.opacity = '0'
-    body.classList.remove('theme1')
-    body.classList.remove('theme3')
-    body.classList.add('theme2')
-})
-thumb3.addEventListener('click', () => {
-    thumb1.style.opacity = '0'
-    thumb2.style.opacity = '0'
-    thumb3.style.opacity = '1'
-    body.classList.remove('theme1')
-    body.classList.remove('theme2')
-    body.classList.add('theme3')
-})
-
-
-
-
-
+thumbs.forEach((thumb, index) => {
+    thumb.addEventListener('click', () => {
+        thumbs.forEach((thumb, i) => {
+            thumb.style.opacity = i === index ? '1' : '0';
+            body.classList.toggle(`theme${i + 1}`, i === index);
+        });
+    });
+});
